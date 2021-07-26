b2cd705679b77241adebb1117eda15a6945bd5d7
FernandoPinage <luizfernandoluck@hotmail.com>
2021-07-26 16:36:33 -0400

    atualizando

--- cdl/resources/views/layout/home_emp.blade.php
+++ cdl/resources/views/layout/home_emp.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-    <link href="/css/home.css" rel="stylesheet">
+    <link href="/css/home_empresa.css" rel="stylesheet">
     <title>Empregabilidade | CDL Manaus</title>
 </head>
 
