4e3b623db239de168c9e3b5e3cb3aa78f379c123
FernandoPinage <luizfernandoluck@hotmail.com>
2021-07-28 17:08:40 -0400

    removendo

--- cdl/resources/views/layout/empresa.blade.php
+++ cdl/resources/views/layout/empresa.blade.php
@@ -119,30 +119,6 @@ Swal.fire({  position: 'center',  icon: 'warning',  title: 'Preencher todos os c
 @endif
 
 
-<!-- Mensagem de cadastro com sucesso -->
-@if(session('mensagem'))
-<script>
-  Swal.fire({
-  position: 'center',
-  icon: 'success',
-  title: 'Registro cadastrado com sucesso!',
-  showConfirmButton: false,
-  timer: 1500
-})
-</script>
-@endif
-
-<!-- Mesagem de validação de campos  -->
-
-@if ($errors->any())
-<script>
-  Swal.fire({  position: 'center',  icon: 'warning',  title: 'Preencha os campos obrigatório',  showConfirmButton: false,  timer: 1500  })
-</script>
-@endif
-
-
-
-
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
 </html>
