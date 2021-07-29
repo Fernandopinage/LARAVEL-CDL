5f21b516deba5c6ae9e98321318ae09f767c1745
FernandoPinage <luizfernandoluck@hotmail.com>
2021-07-29 16:33:59 -0400

    atualizando anuncio de vaga

--- cdl/resources/views/layout/home_emp.blade.php
+++ cdl/resources/views/layout/home_emp.blade.php
@@ -8,6 +8,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
   <link href="/css/home_empresa.css" rel="stylesheet">
+  <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>
   <title>Empregabilidade | CDL Manaus</title>
 </head>
 
@@ -196,6 +197,18 @@
   </footer>
 </body>
 
+<!-- Mensagem de cadastro com sucesso -->
+@if(session('mensagem'))
+  <script>
+  Swal.fire({
+  position: 'center',
+  icon: 'success',
+  title: 'Registro cadastrado com sucesso!',
+  showConfirmButton: false,
+  timer: 1500
+})
+</script>
+@endif
 
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
   integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
