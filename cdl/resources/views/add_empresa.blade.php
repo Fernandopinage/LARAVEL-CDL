3768ed4bb7c29a01ac64827886a9ba415e90b009
FernandoPinage <luizfernandoluck@hotmail.com>
2021-08-03 17:40:39 -0400

    atualização

--- cdl/resources/views/add_empresa.blade.php
+++ cdl/resources/views/add_empresa.blade.php
@@ -26,63 +26,63 @@
 
                 <div class="col-md-4">
                     <label class="form-check-label" for="flexCheckIndeterminate">Nome Fantasia </label>
-                    <input type="text" name="fantasia" class="form-control form-control-sm" placeholder="">
+                    <input type="text" name="fantasia" class="form-control form-control-sm" placeholder=""  value="{{old('fantasia')}}">
                 </div>
 
                 <div class="col-md-4">
                     <label class="form-check-label" for="flexCheckIndeterminate">Razão Social <span><strong>*</strong></span></label>
-                    <input type="text" name="razao" class="form-control form-control-sm" placeholder="">
+                    <input type="text" name="razao" class="form-control form-control-sm" placeholder="" value="{{old('razao')}}">
                 </div>
 
                 <div class="col-md-4">
                     <label class="form-check-label" for="flexCheckIndeterminate">Logo Empresa</label>
-                    <input class="form-control form-control-sm" name="logo" type="file" id="formFile">
+                    <input class="form-control form-control-sm" name="logo" type="file" id="formFile" value="{{old('logo')}}">
                 </div>  
 
                 <div class="col-md-3">
                     <label class="form-check-label" for="flexCheckIndeterminate">CNPJ <span><strong>*</strong></span></label>
-                    <input type="text" class="form-control form-control-sm" onkeypress="return somenteNumeros(event)" onfocus="javascript: retirarFormatacao(this);" onblur="javascript: formatarCampo(this);" name="cnpj" id="cnpj" placeholder="99.999.999/9999-99">
+                    <input type="text" class="form-control form-control-sm" onkeypress="return somenteNumeros(event)" onfocus="javascript: retirarFormatacao(this);" onblur="javascript: formatarCampo(this);" name="cnpj" id="cnpj" placeholder="99.999.999/9999-99" value="{{old('cnpj')}}">
                 </div>
                 <div class="col-md-5">
                     <label class="form-check-label" for="flexCheckIndeterminate">Email da empresa <span><strong>*</strong></span></label>
-                    <input type="text" class="form-control form-control-sm" name="email" placeholder="">
+                    <input type="text" class="form-control form-control-sm" name="email" placeholder="" value="{{old('email')}}">
                 </div>
                 <div class="col-md-2">
                     <label class="form-check-label" for="flexCheckIndeterminate">Senha <span><strong>*</strong></span></label>
-                    <input type="password" class="form-control form-control-sm" name="senha" placeholder="">
+                    <input type="password" class="form-control form-control-sm" name="senha" placeholder="" value="{{old('senha')}}">
                 </div>
                 <div class="col-md-2">
                     <label class="form-check-label" for="flexCheckIndeterminate">Confirmar Senha <span><strong>*</strong></span></label>
-                    <input type="password" class="form-control form-control-sm" name="confirmar" placeholder="">
+                    <input type="password" class="form-control form-control-sm" name="confirmar" placeholder="" value="{{old('confirmar')}}">
                 </div>
                 <div class="col-md-4">
                     <label class="form-check-label" for="flexCheckIndeterminate">Nome do Técnico <span><strong>*</strong></span></label>
-                    <input type="text" class="form-control form-control-sm" name="tecnico" placeholder="">
+                    <input type="text" class="form-control form-control-sm" name="tecnico" placeholder="" value="{{old('tecnico')}}">
                 </div>
                 <div class="col-md-4">
                     <label class="form-check-label" for="flexCheckIndeterminate">Email do Técnico <span><strong>*</strong></span></label>
-                    <input type="text" class="form-control form-control-sm" name="emailtecnico" placeholder="EXEMPLO@EMAIL.COM">
+                    <input type="text" class="form-control form-control-sm" name="emailtecnico" placeholder="EXEMPLO@EMAIL.COM" value="{{old('emailtecnico')}}">
                 </div>
                 <div class="col-md-4">
                     <label class="form-check-label" for="flexCheckIndeterminate">Função/Cargo<span><strong>*</strong></span> </label>
-                    <input type="text" class="form-control form-control-sm" name="funcao" placeholder="">
+                    <input type="text" class="form-control form-control-sm" name="funcao" placeholder="" value="{{old('funcao')}}">
                 </div>
                 <div class="col-md-3">
                     <label class="form-check-label" for="flexCheckIndeterminate">Setor/Departamento <span><strong>*</strong></span></label>
-                    <input type="text" class="form-control form-control-sm" name="setor" placeholder="">
+                    <input type="text" class="form-control form-control-sm" name="setor" placeholder="" value="{{old('setor')}}">
                 </div>
 
                 <div class="col-md-3">
                     <label class="form-check-label" for="flexCheckIndeterminate">Ramo de Atividade <span><strong>*</strong></span></label>
-                    <input type="text" class="form-control form-control-sm" name="ramo" placeholder="">
+                    <input type="text" class="form-control form-control-sm" name="ramo" placeholder="" value="{{old('ramo')}}">
                 </div>
                 <div class="col-md-3">
                     <label class="form-check-label" for="flexCheckIndeterminate">Telefone</label>
-                    <input type="tel" class="form-control form-control-sm" name="telefone" placeholder="(99) 99999-9999" onkeypress="mask(this, mphone);" onblur="mask(this, mphone);" />
+                    <input type="tel" class="form-control form-control-sm" name="telefone" placeholder="(99) 99999-9999" onkeypress="mask(this, mphone);" onblur="mask(this, mphone);" value="{{old('telefone')}}">
                 </div>
                 <div class="col-md-3">
                     <label class="form-check-label" for="flexCheckIndeterminate">Telefone</label>
-                    <input type="tel" class="form-control form-control-sm" name="telefone2" placeholder="(99) 99999-9999" onkeypress="mask(this, mphone);" onblur="mask(this, mphone);" />
+                    <input type="tel" class="form-control form-control-sm" name="telefone2" placeholder="(99) 99999-9999" onkeypress="mask(this, mphone);" onblur="mask(this, mphone);" value="{{old('telefone2')}}">
                 </div>
             </div>  
         </div> 
@@ -109,27 +109,27 @@
         <div class="row g-3">
             <div class="col-md-2">
                 <label class="form-check-label" for="flexCheckIndeterminate">CEP <span><strong>*</strong></span></label>
-                <input type="text" class="form-control form-control-sm" name="cep" id="cep" placeholder="">
+                <input type="text" class="form-control form-control-sm" name="cep" id="cep" placeholder="" value="{{old('cep')}}">
             </div>
             <div class="col-md-1">
                 <label class="form-check-label" for="flexCheckIndeterminate">UF <span><strong>*</strong></span></label>
-                <input type="text" class="form-control form-control-sm" name="uf" id="uf" placeholder="">
+                <input type="text" class="form-control form-control-sm" name="uf" id="uf" placeholder="" value="{{old('uf')}}">
             </div>
             <div class="col-md-2">
                 <label class="form-check-label" for="flexCheckIndeterminate">Cidade <span><strong>*</strong></span></label>
-                <input type="text" class="form-control form-control-sm" name="cidade" id="cidade" placeholder="">
+                <input type="text" class="form-control form-control-sm" name="cidade" id="cidade" placeholder="" value="{{old('cidade')}}">
             </div>
             <div class="col-md-7">
                 <label class="form-check-label" for="flexCheckIndeterminate">Bairro <span><strong>*</strong></span></label>
-                <input type="text" class="form-control form-control-sm" name="bairro" id="bairro" placeholder="">
+                <input type="text" class="form-control form-control-sm" name="bairro" id="bairro" placeholder="" value="{{old('bairro')}}">
             </div>
             <div class="col-md-2">
                 <label class="form-check-label" for="flexCheckIndeterminate">Nº</label>
-                <input type="text" class="form-control form-control-sm" name="numero" id="numero" placeholder="">
+                <input type="text" class="form-control form-control-sm" name="numero" id="numero" placeholder="" value="{{old('numero')}}">
             </div>
             <div class="col-md-10">
                 <label class="form-check-label" for="flexCheckIndeterminate">Complemento</label>
-                <input type="text" class="form-control form-control-sm" name="rua" id="rua" placeholder="">
+                <input type="text" class="form-control form-control-sm" name="rua" id="rua" placeholder="" value="{{old('rua')}}">
             </div>
         </div>
         
@@ -156,13 +156,13 @@
             <div class="col-md-2">
                 <input type="hidden" value="1" name="status"> <!-- campo responsavel por ativar empresa -->
                 <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="" id="asscdl">
+                    <input class="form-check-input" type="checkbox" value="" id="asscdl" value="{{old('razao')}}">
                     <label class="form-check-label" name="associado" id="associado"  for="flexCheckIndeterminate">Associado CDL manaus?</label>
                 </div>
             </div>
             <div class="col-md-4" id="divcod">
                 <label class="form-check-label" for="flexCheckIndeterminate">Preenchar com o código</label>
-                <input type="text" class="form-control form-control-sm" name="cod" id="cod" placeholder="">
+                <input type="text" class="form-control form-control-sm" name="cod" id="cod" placeholder="" value="{{old('cod')}}">
             </div>
             <div class="col-md-6">
             </div>
@@ -186,7 +186,7 @@
                 <div class="text-center">
                     <label class="form-check-label" for="flexCheckIndeterminate">Termo de política <span><strong>*</strong></span></label>
                     <div class="form-check">
-                        <input class="form-check-inpu" type="checkbox" value="S" name="termo" id="">
+                        <input class="form-check-inpu" type="checkbox" value="S" name="termo" id="" value="{{old('termo')}}">
                     </div>
                 </div>
             </div> 
