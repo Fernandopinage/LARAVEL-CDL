7aac6fe6e1bc9b9ae471bcc081b186aa34fc3fb1
FernandoPinage <luizfernandoluck@hotmail.com>
2021-08-06 10:35:39 -0400

    atualizando cdl rede sociais

--- cdl/resources/views/layout/vagas_disponivel.blade.php
+++ cdl/resources/views/layout/vagas_disponivel.blade.php
@@ -55,13 +55,16 @@
           </span>
         </button>
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="https://pt-br.facebook.com/cdlmanausoficial/"><img src="/icons/face.png" width="25px"></a>
+          <a class="nav-link active" aria-current="page" href="https://www.linkedin.com/company/cdlm/"><img src="/icons/1.png" width="25px"></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="https://www.instagram.com/cdlmanaus/?hl=en"><img src="/icons/insta.png" width="25px"></a>
+          <a class="nav-link active" aria-current="page" href="https://pt-br.facebook.com/cdlmanausoficial/"><img src="/icons/2.png" width="25px"></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="https://www.youtube.com/channel/UCyjD5GbQuRrztgyRxlVAZdQ"><img src="/icons/yout.png" width="25px"></a>
+          <a class="nav-link" href="https://www.instagram.com/cdlmanaus/?hl=en"><img src="/icons/4.png" width="25px"></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://www.youtube.com/channel/UCyjD5GbQuRrztgyRxlVAZdQ"><img src="/icons/3.png" width="25px"></a>
         </li>
 
       </ul>
