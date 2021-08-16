7ceaa136d4317da69e699b8dcc7ea5b7a09dc36c
FernandoPinage <luizfernandoluck@hotmail.com>
2021-08-16 11:00:46 -0400

    atualizando cdl

--- cdl/resources/views/update_empresa.blade.php
+++ cdl/resources/views/update_empresa.blade.php
@@ -103,7 +103,7 @@ $empresa;
                         placeholder="99.999.999/9999-99" value="{{old('cnpj')}}" required>
                 </div>
                 <div class="col-md-5">
-                    <label class="formGroupExampleInput" for="flexCheckIndeterminate">Email da empresa
+                    <label class="formGroupExampleInput" for="flexCheckIndeterminate">E-mail da empresa
                         <span><strong>*</strong></span></label>
                     <input type="text"  class="form-control form-control-sm"
                         value="{{$empresa->emp_email}}" name="email" placeholder="" required>
@@ -115,7 +115,7 @@ $empresa;
                         value="{{$empresa->emp_nome_contato}}" placeholder="" required>
                 </div>
                 <div class="col-md-4">
-                    <label class="form-check-label" for="flexCheckIndeterminate">Email do Técnico </label>
+                    <label class="form-check-label" for="flexCheckIndeterminate">E-mail do Técnico </label>
                     <input type="text" class="form-control form-control-sm" name="emailtecnico"
                         value="{{$empresa->emp_email_contato}}" placeholder="EXEMPLO@EMAIL.COM">
                 </div>
@@ -238,7 +238,7 @@ $empresa;
                     <div class="form-check">
                         <input class="form-check-input" type="checkbox" value="" id="asscdl">
                         <label class="form-check-label" name="associado" id="associado"
-                            for="flexCheckIndeterminate">Associado CDL manaus?</label>
+                            for="flexCheckIndeterminate">Associado CDL Manaus?</label>
                     </div>
                 </div>
                 <div class="col-md-4" id="divcod">
@@ -265,29 +265,177 @@ $empresa;
 
         <style>
             .texto {
-                max-height: 250px;
+                max-height: 500px;
                 overflow-y: auto;
                 justify-content: center;
             }
         </style>
 
-        <div class="texto">
-            <p>É um fato conhecido de todos que um leitor se distrairá com o conteúdo de texto legível de uma página
-                quando estiver examinando sua diagramação. A vantagem de usar Lorem Ipsum é que ele tem uma distribuição
-                normal de letras, ao contrário de "Conteúdo aqui, conteúdo aqui", fazendo com que ele tenha uma
-                aparência similar a de um texto legível. Muitos softwares de publicação e editores de páginas na
-                internet agora usam Lorem Ipsum como texto-modelo padrão, e uma rápida busca por 'lorem ipsum' mostra
-                vários websites ainda em sua fase de construção. Várias versões novas surgiram ao longo dos anos,
-                eventualmente por acidente, e às vezes de propósito (injetando humor, e coisas do gênero).</p>
-            <p>Existem muitas variações disponíveis de passagens de Lorem Ipsum, mas a maioria sofreu algum tipo de
-                alteração, seja por inserção de passagens com humor, ou palavras aleatórias que não parecem nem um pouco
-                convincentes. Se você pretende usar uma passagem de Lorem Ipsum, precisa ter certeza de que não há algo
-                embaraçoso escrito escondido no meio do texto. Todos os geradores de Lorem Ipsum na internet tendem a
-                repetir pedaços predefinidos conforme necessário, fazendo deste o primeiro gerador de Lorem Ipsum
-                autêntico da internet. Ele usa um dicionário com mais de 200 palavras em Latim combinado com um punhado
-                de modelos de estrutura de frases para gerar um Lorem Ipsum com aparência razoável, livre de repetições,
-                inserções de humor, palavras não características, etc.</p>
-        </div>
+<div class="texto">
+    <p style="text-align: center;margin-top:10px"><b>SISTEMA DE EMPREGABILIDADE CDL MANAUS</b></p>
+    <p style="text-align: center"><b>Termo de Política para Uso do Sistema</b></p>
+    <p><b>1. SERVIÇO</b></p>
+    <p style="margin-left: 30px">1.1 <b>Objetivo do Serviço:</b>facilitar pessoas interessadas em buscar
+        colocação ou recolocação no
+        mercado de trabalho e empresas interessadas em contratar colaboradores ou ofertar vagas de
+        emprego.</p>
+    <p><b>2. POLÍTICA DE PRIVACIDADE</b></p>
+    <p style="margin-left: 30px">2.1 A presente política de privacidade rege o tratamento dos dados das pessoas,
+        imputados durante
+        o acesso e uso dos serviços que o site CDL Manaus oferece aos seus Usuários. Todas as
+        informações pessoais relativas a membros, clientes, empresas e visitantes, coletadas por meio do
+        Sistema de Empregabilidade CDL Manaus, são em conformidade com a Lei de Proteção de Dados
+        Pessoais (Lei nº 13.709/18), garantindo que qualquer informação fornecida pelos usuários é
+        tratada com diligência e confidencialidade.</p>
+    <p style="margin-left: 30px">2.2 A nossa Política de Privacidade contém informações claras sobre a coleta,
+        uso, armazenamento e
+        proteção de dados dos usuários do serviço. Esta Política se aplica a todos os serviços e aplicações
+        disponibilizados pelo site CDL Manaus, podendo haver termos específicos para determinados
+        serviços, o que será informado oportunamente ao Usuário.
+    </p>
+    <p><b>3. DEFINIÇÕES</b></p>
+    <p style="margin-left: 30px">3.1 CANDIDATO: pessoa interessada em cadastrar-se no Sistema com o propósito de
+        pesquisar
+        oportunidade de emprego;</p>
+    <p style="margin-left: 30px">3.2 EMPRESA: entidade interessada em pesquisar, selecionar candidatos, ou
+        publicar disponibilidade
+        de vagas de emprego;</p>
+    <p style="margin-left: 30px">3.3 MILITARES: organização governamental interessada em cadastrar-se no Sistema
+        com o propósito
+        de proporcionar aos seus integrantes e ex-integrantes a oportunidade de integração ao mercado
+        do trabalho.</p>
+    <p style="margin-left: 30px">3.4 CDL MANAUS: mantenedora do Sistema Empregabilidade;</p>
+    <p style="margin-left: 30px">3.5 UTV: unidade de treinamento e capacitação da CDL Manaus.</p>
+    <p><b>4. OBJETIVO GERAL DA POLÍTICA DE PRIVACIDADE</b></p>
+    <p style="margin-left: 30px">4.1. Facilitar o contato entre candidatos que buscam oportunidades de trabalho
+        e empresas que
+        desejam selecionar profissionais qualificados;</p>
+    <p style="margin-left: 30px">4.2. Servir de ponto de encontro para o sucesso, no qual as empresas encontram
+        os talentos para as
+        suas vagas, e os candidatos acham as melhores empresas e oportunidades para alcançar o êxito
+        profissional.
+    </p>
+    <p><b>5. OBJETIVOS ESPECIFICOS DA POLÍTICA DE PRIVACIDADE</b></p>
+    <p style="margin-left: 30px">5.1. Utilização do SISTEMA para cadastramento de currículo do CANDIDADO, sendo
+        de sua inteira
+        responsabilidade a veracidade e todas informações inseridas no CADASTRO, não tendo a CDL
+        Manaus qualquer reponsabilidade no conteúdo cadastrado;</p>
+    <p style="margin-left: 30px">5.2. Disponibilizar currículos de pessoas habilitadas ou não pela Universidade
+        de Tecnologia do Varejo
+        (UTV), facilitando o acesso ao emprego;</p>
+    <p style="margin-left: 30px">5.3. Captar clientes potenciais com os cursos da UTV / CDL Manaus.
+    </p>
+    <p><b>6. POLÍTICA DE PRIVACIDADE</b></p>
+    <p style="margin-left: 30px">6.1.<b> O que fazemos com as suas informações?</b></p>
+    <p style="margin-left: 45px"><span style="margin-right: 20px; color:#000;font-size:20px;"><b>a</b></span>. Quando
+        você realiza seu cadastro em nossa plataforma, coletamos as informações pessoais
+        que você nos fornece, tais como seu nome, CPF, RG, e-mail, número de telefone e/ou telefone
+        celular, morada, data de nascimento, grau de escolaridade, cursos de aprimoramento,
+        informações profissionais e últimas experiências relacionadas a emprego.</p>
+
+    <p style="margin-left: 45px"><span style="margin-right: 20px; color:#000;font-size:20px;"><b>b</b></span>. De posse
+        dessas informações, partes delas serão distribuídas em nossa página para facilitar a
+        ligação entre você e uma empresa e vice-versa, através de um algoritmo criado
+        especificamente para essa função. Sendo assim, uma empresa ou uma instituição, seja ela
+        pública ou privada terá acesso a seus dados.</p>
+    <p style="margin-left: 45px"><span style="margin-right: 20px; color:#000;font-size:20px;"><b>c</b></span>. A CDL
+        Manaus se compromete a não usar seus dados para outros fins que não sejam única e
+        exclusivamente para intermediar contatos de trabalho entre você e uma empresa e/ou
+        instituição. No entanto a CDL não tem responsabilidade por qualquer vínculo empregatício
+        ou acordo firmado entre as partes.</p>
+
+    <p style="margin-left: 45px"><span style="margin-right: 20px; color:#000;font-size:20px;"><b>d</b></span>. É vedado
+        o acesso total de empresas não cadastradas aos seus dados.</p>
+    <p style="margin-left: 45px"><span style="margin-right: 20px; color:#000;font-size:20px;"><b>e</b></span>. Se faz
+        necessário ressaltar, que ao aceitar nossa política de privacidade, você nos autoriza a
+        informá-lo sobre nossos serviços, produtos e outras atualizações por e-mail, sms ou outro
+        meio eletrônico.
+    </p>
+    <p style="margin-left: 45px"><span style="margin-right: 20px; color:#000;font-size:20px;"><b>f</b></span>. Todas as
+        informações pessoais relativas a membros, assinantes, clientes ou visitantes que
+        usem o portal de empregabilidade serão tratadas em concordância com a Lei da Proteção de
+        Dados Pessoais (Lei nº 13.709/18).</p>
+    <p style="margin-left: 45px"><span style="margin-right: 20px; color:#000;font-size:20px;"><b>g</b></span>. O uso do
+        sistema de empregabilidade CDL MANAUS pressupõe a aceitação deste acordo de
+        privacidade. A CDL MANAUS reserva-se ao direito de alterar este acordo sem aviso prévio.
+        Deste modo, recomendamos que consulte a nossa política de privacidade com regularidade
+        de forma a estar sempre atualizado.</p>
+
+    <p><b>6.2. Os anúncios de oferta de vagas e programação de treinamento e capacitação da CDL Manaus.
+        </b></p>
+    <p style="margin-left: 45px"><span style="margin-right: 20px; color:#000;font-size:20px;"><b>a</b></span>. A
+        política de anúncios do sistema de Empregabilidade CDL Manaus, restringe sua publicidade
+        de anúncios ao tempo da sua visita e as páginas que visitou dentro do nosso website. </p>
+    <p><b>6.3. Cookies</b></p>
+    <p style="margin-left: 45px"><span style="margin-right: 20px; color:#000;font-size:20px;"><b>a</b></span>. A CDL
+        MANAUS, não utiliza cookies muito menos armazena dados de clientes para
+        comercializar ou exibir anúncios. </p>
+    <p style="margin-left: 45px"><span style="margin-right: 20px; color:#000;font-size:20px;"><b>b</b></span>. Os
+        cookies rastreáveis são ou pode ser partes referentes as ligações de uso dos sites das redes
+        sociais. (Facebook, Instagram, Youtube e outros)</p>
+
+    <p><b>6.4. Ligações a sites de terceiros</b></p>
+
+    <p style="margin-left: 45px"><span style="margin-right: 20px; color:#000;font-size:20px;"><b>a</b></span>. O CDL
+        MANAUS possui ligações para outros sites, os quais, ao nosso ver, podem conter
+        informações / ferramentas úteis para os nossos visitantes. A nossa política de privacidade não
+        é aplicada a sites de terceiros, pelo que, caso visite outro site a partir do nosso deverá ler a
+        política de privacidade do mesmo. </p>
+    <p style="margin-left: 45px"><span style="margin-right: 20px; color:#000;font-size:20px;"><b>b</b></span>. A CDL
+        MANAUS não se responsabiliza pela política de privacidade ou conteúdo presente nos
+        sites de terceiros.</p>
+
+    <p><b>7. DO CONSENTIMENTO DE USO DE SUAS INFORMAÇÕES</b></p>
+    <p style="margin-left: 30px">7.1 Quando você nos fornece as suas informações pessoais para completar seu
+        cadastro,
+        automaticamente está concordando com a nossa coleta e uso de informações pessoais apenas
+        para esses fins específicos.
+    </p>
+    <p style="margin-left: 30px">7.2 <b>Como posso retirar os meus dados da lista de marketing?</b> </p>
+    <p style="margin-left: 45px"><span style="margin-right: 20px; color:#000;font-size:20px;"><b>a</b></span>. Podemos
+        divulgar suas informações pessoais se formos obrigados por lei a fazê-lo (para
+        autoridades competentes) ou se você violar nossos Termos de Serviço. </p>
+
+    <p><b>8. DA SEGURANÇA DE SEUS DADOS</b></p>
+    <p style="margin-left: 30px">8.1 <b>Links Internos e Links Externos</b><br>Quando você nos fornece as suas
+        informações pessoais para completar seu cadastro,
+        automaticamente está concordando com a nossa coleta e uso de informações pessoais apenas
+        para esses fins específicos.
+    </p>
+    <p><b>9. DA IDADE DE CONSENTIMENTO</b></p>
+    <p style="margin-left: 30px">9.1. Ao usar esse site e cadastrar suas informações, você confirma que é maior
+        de idade ou que
+        realizou seu cadastro sob a orientação de uma pessoa responsável maior idade, e que se localizava
+        em seu estado ou província de residência e que nos deu seu consentimento para armazenar seus
+        dados pessoais.
+    </p>
+
+    <p><b>10. DAS ALTERAÇÕES NA POLÍTICA DE PRIVACIDADE </b></p>
+    <p style="margin-left: 30px">10.1. Reservamos o direito de modificar essa política de privacidade a qualquer
+        momento. Portanto,
+        por favor, leia-a com frequência. As alterações e esclarecimentos surtem efeito imediatamente
+        após serem publicadas no site. Caso façamos alterações na política de privacidade, iremos notificálo
+        sobre a atualização. Assim, você saberá quais informações coletamos, como as usamos, e sob
+        que circunstâncias, caso aplicável, as utilizaremos e/ou divulgaremos.
+
+    </p>
+    <p style="margin-left: 30px">10.2. Caso ocorra a fusão de nosso site com outra ferramenta da empresa, suas
+        informações podem ser
+        transferidas para os novos bancos de dados para que possamos continuar acessando e ofertando
+        nossos serviços e produtos para você.
+
+    </p>
+
+    <p><b>11. PERGUNTAS E INFORMAÇÕES DE CONTATO </b></p>
+    <p style="margin-left: 30px">11.1. Se você gostaria de acessar, corrigir, alterar ou excluir quaisquer
+        informações pessoais que temos
+        sobre você, registre uma queixa, ou simplesmente peça mais informações de contato conosco
+        através do e-mail suporte@cdlmanaus.org.br ou por telefone: (92) 3627-2894.
+
+    </p>
+
+
+</div>
 
         <div class="form-check" style="margin-top: 50px">
             <input class="form-check-input" type="checkbox" value="{{$empresa->emp_termo}}" name="termo" id="termo"
