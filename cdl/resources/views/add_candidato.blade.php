b2dcd49a798adacf574dd37dcff05ec307863bcd
FernandoPinage <luizfernandoluck@hotmail.com>
2021-09-16 14:21:28 -0400

    add candidato

--- cdl/resources/views/add_candidato.blade.php
+++ cdl/resources/views/add_candidato.blade.php
@@ -2,7 +2,7 @@
 
 @section('cadastro')
 
-<form action="empresa"  method="POST" >
+<form action="empresa" method="POST">
 
     @csrf
     <div class="form-signin">
@@ -12,50 +12,50 @@
                 <span class="slider round"></span>
             </label>
         </div>
-        
-        <div class="text" >          
+
+        <div class="text">
             <h2 class="form-signin-heading">Dados Pessoais</h2>
             <hr>
         </div>
-        
+
         <div class="pessoais">
 
             <div class="row g-3">
 
                 <div class="col-md-4">
                     <label class="form-check-label" for="flexCheckIndeterminate">Primeiro nome </label>
-                    <input type="text" class="form-control" name="nome" placeholder="">
+                    <input type="text" class="form-control form-control-sm" name="nome" placeholder="">
                 </div>
 
                 <div class="col-md-4">
                     <label class="form-check-label" for="flexCheckIndeterminate">Sobre Nome</label>
-                    <input type="text" class="form-control" name="sobrenome" placeholder="">
+                    <input type="text" class="form-control form-control-sm" name="sobrenome" placeholder="">
                 </div>
 
                 <div class="col-md-4">
                     <label class="form-check-label" for="flexCheckIndeterminate">Selecione Sua foto</label>
-                    <input class="form-control" type="file" name="foto" id="formFile">
-                </div>  
+                    <input class="form-control form-control-sm" type="file" name="foto" id="formFile">
+                </div>
 
                 <div class="col-md-3">
                     <label class="form-check-label" for="flexCheckIndeterminate">CPF</label>
-                    <input type="text" class="form-control" name="cpf" placeholder="">
+                    <input type="text" class="form-control form-control-sm" name="cpf" placeholder="">
                 </div>
                 <div class="col-md-3">
                     <label class="form-check-label" for="flexCheckIndeterminate">RG</label>
-                    <input type="text" class="form-control" name="rg" placeholder="">
+                    <input type="text" class="form-control form-control-sm" name="rg" placeholder="">
                 </div>
                 <div class="col-md-3">
                     <label class="form-check-label" for="flexCheckIndeterminate">Data Nascimento </label>
-                    <input type="date" class="form-control" name="nascimento" placeholder="">
+                    <input type="date" class="form-control form-control-sm" name="nascimento" placeholder="">
                 </div>
                 <div class="col-md-3">
                     <label class="form-check-label" for="flexCheckIndeterminate">Local de Nascimento </label>
-                    <input type="text" class="form-control" name="localnascimento" placeholder="">
+                    <input type="text" class="form-control form-control-sm" name="localnascimento" placeholder="">
                 </div>
                 <div class="col-md-2">
                     <label class="form-check-label" for="flexCheckIndeterminate">Cor</label>
-                    <select id="inputState" name="cor" class="form-select">
+                    <select id="inputState" name="cor" class="form-select form-select-sm">
                         <option selected></option>
                         <option>Branca</option>
                         <option>Preta</option>
@@ -73,30 +73,52 @@
                     <input type="email" class="form-control" placeholder="">
                 </div>
                 <div class="col-md-2">
-                    <label class="form-check-label" for="flexCheckIndeterminate">Gênero </label>
+                    <label class="form-check-label" for="flexCheckIndeterminate">Estado Civil </label>
+                    <select id="inputState" name="cor" class="form-select form-select-sm">
+                        <option selected></option>
+                        <option>Solteiro</option>
+                        <option>Casado</option>
+                        <option>Viúvo</option>
+                        <option>Separado</option>
+                    </select>
+                </div>
+                <div class="col-md-1">
+                    <label class="form-check-label" for="flexCheckIndeterminate">Estado Civil </label>
                     <div class="form-check ">
                         <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2">
                         <label class="form-check-label" for="gridRadios2">
-                        Homem
+                            Homem
                         </label>
                     </div>
                     <div class="form-check ">
                         <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="option3">
                         <label class="form-check-label" for="gridRadios3">
-                        Mulher
+                            Mulher
                         </label>
                     </div>
                     <div class="form-check ">
                         <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="option3">
                         <label class="form-check-label" for="gridRadios3">
-                        Outros
+                            Outros
                         </label>
                     </div>
                 </div>
+                <div class="col-md-2">
+                    <label class="form-check-label" for="flexCheckIndeterminate">Sexo</label>
+                    <select id="inputState" name="cor" class="form-select form-select-sm">
+                        <option selected></option>
+                        <option>Masculino</option>
+                        <option>feminino</option>
+                        <option>outros</option>
+                        
+                    </select>
+                </div>
+            </div>
+            </div>
+        </div>
 
-            </div>  
-        </div> 
-    </div>
+
+
     <div class="form-signin">
         <div class="text-end">
             <label class="switch">
@@ -105,10 +127,10 @@
             </label>
         </div>
         <div class="row g-3">
-        <div class="text" >
-            <h2 class="form-signin-heading">Informações de contato</h2>
-            <hr>
-        </div>
+            <div class="text">
+                <h2 class="form-signin-heading">Informações de contato</h2>
+                <hr>
+            </div>
 
             <div class="col-md-4">
                 <label class="form-check-label" for="flexCheckIndeterminate">Email </label>
@@ -133,8 +155,6 @@
 
         </div>
 
-   
-
     </div>
 
 
@@ -145,12 +165,12 @@
                 <span class="slider round"></span>
             </label>
         </div>
-    
-        <div class="text" >
+
+        <div class="text">
             <h2 class="form-signin-heading">Endereço</h2>
             <hr>
         </div>
-            
+
         <div class="row g-3">
             <div class="col-md-2">
                 <label class="form-check-label" for="flexCheckIndeterminate">CEP</label>
@@ -177,10 +197,10 @@
                 <input type="text" class="form-control" placeholder="">
             </div>
         </div>
-       
-    
+
+
     </div>
-    
+
 </form>
 
 
