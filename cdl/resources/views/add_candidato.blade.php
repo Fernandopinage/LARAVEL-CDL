4e7d4d488aa2131466f2f33be973244718623a03
FernandoPinage <luizfernandoluck@hotmail.com>
2021-09-18 09:47:37 -0400

    atualizando cdl

--- cdl/resources/views/add_candidato.blade.php
+++ cdl/resources/views/add_candidato.blade.php
@@ -28,7 +28,7 @@
                 </div>
 
                 <div class="col-md-4">
-                    <label class="form-check-label" for="flexCheckIndeterminate">SobreNome</label>
+                    <label class="form-check-label" for="flexCheckIndeterminate">Sobrenome</label>
                     <input type="text" class="form-control form-control-sm" name="sobrenome" placeholder="">
                 </div>
 
@@ -39,11 +39,13 @@
 
                 <div class="col-md-3">
                     <label class="form-check-label" for="flexCheckIndeterminate">CPF</label>
-                    <input type="text" class="form-control form-control-sm" name="cpf" placeholder="">
+                    <input type="text" class="form-control form-control-sm" name="cpf" placeholder="" onfocus="javascript: retirarFormatacao(this);"
+                    onblur="javascript: formatarCampo(this);" name="cnpj" id="cnpj" placeholder="99.999.999/9999-99"
+                    value="{{old('cnpj')}}">
                 </div>
                 <div class="col-md-3">
                     <label class="form-check-label" for="flexCheckIndeterminate">RG</label>
-                    <input type="text" class="form-control form-control-sm" name="rg" placeholder="">
+                    <input type="text" maxlength="10" class="form-control form-control-sm" onkeyup="somenteNumeros(this);" name="rg" placeholder="">
                 </div>
                 <div class="col-md-3">
                     <label class="form-check-label" for="flexCheckIndeterminate">Data Nascimento </label>
@@ -59,18 +61,18 @@
                         <option selected></option>
                         <option>Branca</option>
                         <option>Preta</option>
-                        <option>Parta</option>
+                        <option>Parda </option>
                         <option>Amarela</option>
                         <option>Indígena</option>
                     </select>
                 </div>
                 <div class="col-md-1">
                     <label class="form-check-label" for="flexCheckIndeterminate">Peso</label>
-                    <input type="email" class="form-control form-control-sm" placeholder="">
+                    <input type="text" class="form-control form-control-sm" placeholder="">
                 </div>
                 <div class="col-md-1">
                     <label class="form-check-label" for="flexCheckIndeterminate">Altura</label>
-                    <input type="email" class="form-control form-control-sm" placeholder="">
+                    <input type="text" class="form-control form-control-sm" placeholder="">
                 </div>
                 <div class="col-md-2">
                     <label class="form-check-label" for="flexCheckIndeterminate">Estado Civil </label>
@@ -153,11 +155,11 @@
             </div>
             <div class="col-md-2">
                 <label class="form-check-label" for="flexCheckIndeterminate">Telefone</label>
-                <input type="text" class="form-control form-control-sm" placeholder="">
+                <input type="text" class="form-control form-control-sm" placeholder="(xx) xxxxx-xxxx" onkeypress="mask(this, mphone);" onblur="mask(this, mphone);" value="{{old('telefone')}}">
             </div>
             <div class="col-md-2">
                 <label class="form-check-label" for="flexCheckIndeterminate">Celular </label>
-                <input type="text" class="form-control form-control-sm" placeholder="">
+                <input type="text" class="form-control form-control-sm" placeholder="(xx) xxxxx-xxxx" onkeypress="mask(this, mphone);" onblur="mask(this, mphone);" value="{{old('telefone')}}">
             </div>
 
         </div>
@@ -178,31 +180,50 @@
             <hr>
         </div>
 
-        <div class="row g-3">
-            <div class="col-md-2">
-                <label class="form-check-label" for="flexCheckIndeterminate">CEP</label>
-                <input type="text" class="form-control form-control-sm" placeholder="">
+        <div class="endereco">
+            <div class="row g-3">
+                <div class="col-md-2">
+                    <label class="form-check-label" for="flexCheckIndeterminate">CEP
+                        <span><strong>*</strong></span></label>
+                    <input type="text"  maxlength="9"  class="form-control form-control-sm" name="cep" id="cep" placeholder=""
+                        value="{{old('cep')}}" required>
+                </div>
+                <div class="col-md-1">
+                    <label class="form-check-label" for="flexCheckIndeterminate">UF
+                        <span><strong>*</strong></span></label>
+                    <input type="text" class="form-control form-control-sm" maxlength="2" name="uf" id="uf" placeholder=""
+                        value="{{old('uf')}}" required onkeypress="return lettersOnly(event);">
+                </div>
+                <div class="col-md-2">
+                    <label class="form-check-label" for="flexCheckIndeterminate">Cidade
+                        <span><strong>*</strong></span></label>
+                    <input type="text" class="form-control form-control-sm" name="cidade" id="cidade" placeholder=""
+                        value="{{old('cidade')}}" required>
+                </div>
+                <div class="col-md-7">
+                    <label class="form-check-label" for="flexCheckIndeterminate">Bairro
+                        <span><strong>*</strong></span></label>
+                    <input type="text" class="form-control form-control-sm" name="bairro" id="bairro" placeholder=""
+                        value="{{old('bairro')}}" required>
+                </div>
+                <div class="col-md-2">
+                    <label class="form-check-label"
+                        for="flexCheckIndeterminate">Nº</label></label><span><strong>*</strong></span></label>
+                    <input type="text" class="form-control form-control-sm" name="numero" id="numero" placeholder=""
+                        value="{{old('numero')}}" required>
+                </div>
+                <div class="col-md-4">
+                    <label class="form-check-label" for="flexCheckIndeterminate">Logradouro</label>
+                    <input type="text" class="form-control form-control-sm" name="rua" id="rua" placeholder=""
+                        value="{{old('rua')}}">
+                </div>
+                <div class="col-md-6">
+                    <label class="form-check-label" for="flexCheckIndeterminate">Complemento</label>
+                    <input type="text" class="form-control form-control-sm" name="complemento" id="complemento"
+                        placeholder="" value="{{old('complemento')}}">
+                </div>
             </div>
-            <div class="col-md-1">
-                <label class="form-check-label" for="flexCheckIndeterminate">UF</label>
-                <input type="text" class="form-control form-control-sm" placeholder="">
-            </div>
-            <div class="col-md-2">
-                <label class="form-check-label" for="flexCheckIndeterminate">Cidade</label>
-                <input type="text" class="form-control form-control-sm" placeholder="">
-            </div>
-            <div class="col-md-7">
-                <label class="form-check-label" for="flexCheckIndeterminate">Bairro</label>
-                <input type="text" class="form-control form-control-sm" placeholder="">
-            </div>
-            <div class="col-md-2">
-                <label class="form-check-label" for="flexCheckIndeterminate">Nº</label>
-                <input type="text" class="form-control form-control-sm" placeholder="">
-            </div>
-            <div class="col-md-10">
-                <label class="form-check-label" for="flexCheckIndeterminate">Complemento</label>
-                <input type="text" class="form-control form-control-sm" placeholder="">
-            </div>
+
         </div>
 
 
@@ -404,7 +425,7 @@
         </div>
         <div class="row g-3">
             <div class="text">
-                <h2 class="form-signin-heading">Preferências Profissional</h2>
+                <h2 class="form-signin-heading">Preferencias Profissionais</h2>
                 <hr>
             </div>
         </div>
@@ -681,3 +702,182 @@
          document.getElementById('divUTv'+id).remove();   
     }
 </script>
+
+
+
+<!-- *************************validando cnpj ************************* -->
+<script>
+    function formatarCampo(campoTexto) {
+        if (campoTexto.value.length <= 11) {
+            campoTexto.value = mascaraCpf(campoTexto.value);
+        } else {
+            campoTexto.value = mascaraCnpj(campoTexto.value);
+        }
+    }
+    function retirarFormatacao(campoTexto) {
+        campoTexto.value = campoTexto.value.replace(/(\.|\/|\-)/g,"");
+    }
+    function mascaraCpf(valor) {
+        return valor.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g,"\$1.\$2.\$3\-\$4");
+    }
+    function mascaraCnpj(valor) {
+        return valor.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/g,"\$1.\$2.\$3\/\$4\-\$5");
+    }
+
+    function somenteNumeros(e) {
+        var charCode = e.charCode ? e.charCode : e.keyCode;
+        // charCode 8 = backspace   
+        // charCode 9 = tab
+        if (charCode != 8 && charCode != 9) {
+            // charCode 48 equivale a 0   
+            // charCode 57 equivale a 9
+            if (charCode < 48 || charCode > 57) {
+                return false;
+            }
+        }
+    }
+</script>
+<!-- *************************validando telefone ************** -->
+
+<script>
+    function mouseDown() {
+  document.getElementById("termo_politica").style.color = "red";
+}
+
+function mouseUp() {
+  document.getElementById("termo_politica").style.color = "green";
+}
+
+
+</script>
+
+
+<script type="text/javascript">
+    function mask(o, f) {
+  setTimeout(function() {
+    var v = mphone(o.value);
+    if (v != o.value) {
+      o.value = v;
+    }
+  }, 1);
+}
+
+function mphone(v) {
+    var r = v.replace(/\D/g, "");
+    r = r.replace(/^0/, "");
+    if (r.length > 10) {
+        r = r.replace(/^(\d\d)(\d{5})(\d{4}).*/, "($1) $2-$3");
+    } else if (r.length > 5) {
+        r = r.replace(/^(\d\d)(\d{4})(\d{0,4}).*/, "($1) $2-$3");
+    } else if (r.length > 2) {
+        r = r.replace(/^(\d\d)(\d{0,5})/, "($1) $2");
+    } else {
+        r = r.replace(/^(\d*)/, "($1");
+    }
+    return r;
+    }
+</script>
+
+<!-- ----------------------------------------------------------------- -->
+    <!-- Adicionando JQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+            integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+            crossorigin="anonymous"></script>
+<!-- Adicionando Javascript -->
+<script>
+    $(document).ready(function() {
+  
+          function limpa_formulário_cep() {
+              // Limpa valores do formulário de cep.
+              $("#rua").val("");
+              $("#bairro").val("");
+              $("#cidade").val("");
+              $("#uf").val("");
+              $("#ibge").val("");
+          }
+  
+          //Quando o campo cep perde o foco.
+          $("#cep").blur(function() {
+  
+              //Nova variável "cep" somente com dígitos.
+              var cep = $(this).val().replace(/\D/g, '');
+  
+              //Verifica se campo cep possui valor informado.
+              if (cep != "") {
+  
+                  //Expressão regular para validar o CEP.
+                  var validacep = /^[0-9]{8}$/;
+  
+                  //Valida o formato do CEP.
+                  if (validacep.test(cep)) {
+  
+                      //Preenche os campos com "..." enquanto consulta webservice.
+                      $("#rua").val("...");
+                      $("#bairro").val("...");
+                      $("#cidade").val("...");
+                      $("#uf").val("...");
+                      $("#ibge").val("...");
+  
+                      //Consulta o webservice viacep.com.br/
+                      $.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?", function(dados) {
+  
+                          if (!("erro" in dados)) {
+                              //Atualiza os campos com os valores da consulta.
+                              $("#rua").val(dados.logradouro);
+                              $("#bairro").val(dados.bairro);
+                              $("#cidade").val(dados.localidade);
+                              $("#uf").val(dados.uf);
+                              $("#ibge").val(dados.ibge);
+                          } //end if.
+                          else {
+                              //CEP pesquisado não foi encontrado.
+                              limpa_formulário_cep();
+                              alert("CEP não encontrado.");
+                          }
+                      });
+                  } //end if.
+                  else {
+                      //cep é inválido.
+                      limpa_formulário_cep();
+                      alert("Formato de CEP inválido.");
+                  }
+              } //end if.
+              else {
+                  //cep sem valor, limpa formulário.
+                  limpa_formulário_cep();
+              }
+          });
+      });
+  </script>
+  
+  <!--  Script campo RG onde só aceita numero -->
+
+  <script>
+    function somenteNumeros(num) {
+        var er = /[^0-9.]/;
+        er.lastIndex = 0;
+        var campo = num;
+        if (er.test(campo.value)) {
+          campo.value = "";
+        }
+    }
+ </script>
+  
+  <!---------------------------------------------------->
+
+
+  <!-- VALIDANDO DADOS APENAS LETRAS -->
+  <script>
+      function lettersOnly(evt) {
+    evt = (evt) ? evt : event;
+    var charCode = (evt.charCode) ? evt.charCode : ((evt.keyCode) ? evt.keyCode :
+        ((evt.which) ? evt.which : 0));
+    if (charCode > 31 && (charCode < 65 || charCode > 90) &&
+        (charCode < 97 || charCode > 122)) {
+        
+        return false;
+    }
+    return true;
+}
+  </script>
+  <!--------------------------------------------------->
