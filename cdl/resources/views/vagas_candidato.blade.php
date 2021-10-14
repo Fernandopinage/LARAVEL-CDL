68c98dbbb666a0809ea7b971390954653c95f9a1
FernandoPinage <luizfernandoluck@hotmail.com>
2021-10-14 14:16:22 -0400

    ok cdl

--- cdl/resources/views/vagas_candidato.blade.php
+++ cdl/resources/views/vagas_candidato.blade.php
@@ -2,6 +2,22 @@
 
 @section('vagas')
 
-<h1>vagas</h1>
+<form class="form-signin" method="get" action="" style="margin-top:50px">
+    <div class="text-center">
+        <h2 class="form-signin-heading">Buscar Vagas</h2>
+        <hr>
+    </div>
+    <div class="row" class="g-3">
+        <div class="col-sm-2">
+          <input type="text" class="form-control" placeholder="Cidade ou Estado " aria-label="Pesquisar cargo, competência ou empresa">
+        </div>
+        <div class="col-sm-8">
+          <input type="text" class="form-control" placeholder="Pesquisar cargo, competência ou empresa"  aria-label="Last name">
+        </div>
+        <div class="col-sm-2">
+            <button type="submit" class="btn btn-primary">Sign in</button>
+        </div>
+    </div>
+</form>
 
 @endsection
