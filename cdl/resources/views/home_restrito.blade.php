2d82172fcdb4f1996ffd0742aae80e42c0d2ad20
FernandoPinage <luizfernandoluck@hotmail.com>
2021-11-03 10:20:36 -0400

    ok

--- cdl/resources/views/home_restrito.blade.php
+++ cdl/resources/views/home_restrito.blade.php
@@ -2,7 +2,7 @@
 
 @section('home')
 
-<form action="" method="GET"  enctype="multipart/form-data" style="margin-bottom: 300px">
+<form action="" method="GET"  enctype="multipart/form-data" >
 
     <div class="row g-3 shadow p-4 mb-5 bg-body rounded" style="margin-top: 80px">
         <div class="text-center">
@@ -96,7 +96,7 @@
 
 </form>
 
-<form action="" method="GET" enctype="multipart/form-data" style="margin-bottom: 300px">
+<form action="" method="GET" enctype="multipart/form-data" >
 
     <div class="row g-3 shadow p-4 mb-5 bg-body rounded" style="margin-top: 80px">
         <div class="text-center">
@@ -108,17 +108,17 @@
             <div class="col-md-4" id="selecionarFoto">
                 <label class="form-check-label" for="image">Foto do banner principal</label>
                 <!--<input class="form-control form-control-sm" type="file" name="foto" id="formFile"> -->
-                <input type="file" name="foto" id="formFile" class="form-control-file">
+                <input type="file" name="foto_candidato" id="formFile" class="form-control-file">
             </div>
             <div class="col-md-2">
                 <label class="form-check-label" for="flexCheckIndeterminate">Altura
                     <span><strong>*</strong></span> </label>
-                <input type="number" class="form-control form-control-sm" name="nome" placeholder="">
+                <input type="number" class="form-control form-control-sm" name="altura_candidato" placeholder="">
             </div>
             <div class="col-md-2">
                 <label class="form-check-label" for="flexCheckIndeterminate">Largura
                     <span><strong>*</strong></span> </label>
-                <input type="number" class="form-control form-control-sm" name="nome" placeholder="">
+                <input type="number" class="form-control form-control-sm" name="largura_candidato" placeholder="">
             </div>
             <div class="text-end" style="margin-top: 20px">
                 <input type="submit" class="btn btn-success" value="Salvar Registros">
@@ -128,7 +128,7 @@
 
 </form>
 
-<form action="" method="GET"  enctype="multipart/form-data" style="margin-bottom: 300px">
+<form action="" method="GET"  enctype="multipart/form-data">
 
     <div class="row g-3 shadow p-4 mb-5 bg-body rounded" style="margin-top: 80px">
         <div class="text-center">
@@ -141,17 +141,17 @@
             <div class="col-md-4" id="selecionarFoto">
                 <label class="form-check-label" for="image">Foto do banner principal</label>
                 <!--<input class="form-control form-control-sm" type="file" name="foto" id="formFile"> -->
-                <input type="file" name="foto" id="formFile" class="form-control-file">
+                <input type="file" name="foto_empresa" id="formFile" class="form-control-file">
             </div>
             <div class="col-md-2">
                 <label class="form-check-label" for="flexCheckIndeterminate">Altura
                     <span><strong>*</strong></span> </label>
-                <input type="number" class="form-control form-control-sm" name="nome" placeholder="">
+                <input type="number" class="form-control form-control-sm" name="altura_empresa" placeholder="">
             </div>
             <div class="col-md-2">
                 <label class="form-check-label" for="flexCheckIndeterminate">Largura
                     <span><strong>*</strong></span> </label>
-                <input type="number" class="form-control form-control-sm" name="nome" placeholder="">
+                <input type="number" class="form-control form-control-sm" name="largura_empresa" placeholder="">
             </div>
 
         </div>
@@ -162,17 +162,17 @@
             <div class="col-md-4" id="selecionarFoto">
                 <label class="form-check-label" for="image">Foto do banner principal</label>
                 <!--<input class="form-control form-control-sm" type="file" name="foto" id="formFile"> -->
-                <input type="file" name="foto" id="formFile" class="form-control-file">
+                <input type="file" name="foto_empresa_home" id="formFile" class="form-control-file">
             </div>
             <div class="col-md-2">
                 <label class="form-check-label" for="flexCheckIndeterminate">Altura
                     <span><strong>*</strong></span> </label>
-                <input type="number" class="form-control form-control-sm" name="nome" placeholder="">
+                <input type="number" class="form-control form-control-sm" name="altura02_empresa" placeholder="">
             </div>
             <div class="col-md-2">
                 <label class="form-check-label" for="flexCheckIndeterminate">Largura
                     <span><strong>*</strong></span> </label>
-                <input type="number" class="form-control form-control-sm" name="nome" placeholder="">
+                <input type="number" class="form-control form-control-sm" name="largura02_empresa" placeholder="">
             </div>
             <div class="text-end" style="margin-top: 20px">
                 <input type="submit" class="btn btn-success" value="Salvar Registros">
@@ -184,7 +184,7 @@
 </form>
 
 
-<form action="" method="GET"  enctype="multipart/form-data" style="margin-bottom: 300px">
+<form action="" method="GET"  enctype="multipart/form-data" >
 
     <div class="row g-3 shadow p-4 mb-5 bg-body rounded" style="margin-top: 80px">
         <div class="text-center">
@@ -197,17 +197,17 @@
             <div class="col-md-4" id="selecionarFoto">
                 <label class="form-check-label" for="image">Foto do banner principal</label>
                 <!--<input class="form-control form-control-sm" type="file" name="foto" id="formFile"> -->
-                <input type="file" name="foto" id="formFile" class="form-control-file">
+                <input type="file" name="foto_oms" id="formFile" class="form-control-file">
             </div>
             <div class="col-md-2">
                 <label class="form-check-label" for="flexCheckIndeterminate">Altura
                     <span><strong>*</strong></span> </label>
-                <input type="number" class="form-control form-control-sm" name="nome" placeholder="">
+                <input type="number" class="form-control form-control-sm" name="altura_oms" placeholder="">
             </div>
             <div class="col-md-2">
                 <label class="form-check-label" for="flexCheckIndeterminate">Largura
                     <span><strong>*</strong></span> </label>
-                <input type="number" class="form-control form-control-sm" name="nome" placeholder="">
+                <input type="number" class="form-control form-control-sm" name="largura_oms" placeholder="">
             </div>
 
         </div>
@@ -218,17 +218,17 @@
             <div class="col-md-4" id="selecionarFoto">
                 <label class="form-check-label" for="image">Foto do banner principal</label>
                 <!--<input class="form-control form-control-sm" type="file" name="foto" id="formFile"> -->
-                <input type="file" name="foto" id="formFile" class="form-control-file">
+                <input type="file" name="foto_slide_oms" id="formFile" class="form-control-file">
             </div>
             <div class="col-md-2">
                 <label class="form-check-label" for="flexCheckIndeterminate">Altura
                     <span><strong>*</strong></span> </label>
-                <input type="number" class="form-control form-control-sm" name="nome" placeholder="" required>
+                <input type="number" class="form-control form-control-sm" name="altura01_oms" placeholder="" required>
             </div>
             <div class="col-md-2">
                 <label class="form-check-label" for="flexCheckIndeterminate">Largura
                     <span><strong>*</strong></span> </label>
-                <input type="number" class="form-control form-control-sm" name="nome" placeholder="" required>
+                <input type="number" class="form-control form-control-sm" name="largura01_oms" placeholder="" required>
             </div>
 
         </div>
@@ -239,17 +239,17 @@
             <div class="col-md-4" id="selecionarFoto">
                 <label class="form-check-label" for="image">Foto do banner principal</label>
                 <!--<input class="form-control form-control-sm" type="file" name="foto" id="formFile"> -->
-                <input type="file" name="foto" id="formFile" class="form-control-file">
+                <input type="file" name="foto_slide02_oms" id="formFile" class="form-control-file">
             </div>
             <div class="col-md-2">
                 <label class="form-check-label" for="flexCheckIndeterminate">Altura
                     <span><strong>*</strong></span> </label>
-                <input type="number" class="form-control form-control-sm" name="nome" placeholder="" required>
+                <input type="number" class="form-control form-control-sm" name="altura02_oms" placeholder="" required>
             </div>
             <div class="col-md-2">
                 <label class="form-check-label" for="flexCheckIndeterminate">Largura
                     <span><strong>*</strong></span> </label>
-                <input type="number" class="form-control form-control-sm" name="nome" placeholder="" required>
+                <input type="number" class="form-control form-control-sm" name="largura02_oms" placeholder="" required>
             </div>
         </div>
 
@@ -259,17 +259,17 @@
             <div class="col-md-4" id="selecionarFoto">
                 <label class="form-check-label" for="image">Foto do banner principal</label>
                 <!--<input class="form-control form-control-sm" type="file" name="foto" id="formFile"> -->
-                <input type="file" name="foto" id="formFile" class="form-control-file">
+                <input type="file" name="fofoto_slide03_omsto" id="formFile" class="form-control-file">
             </div>
             <div class="col-md-2">
                 <label class="form-check-label" for="flexCheckIndeterminate">Altura
                     <span><strong>*</strong></span> </label>
-                <input type="number" class="form-control form-control-sm" name="nome" placeholder="" required>
+                <input type="number" class="form-control form-control-sm" name="altura03_oms" placeholder="" required>
             </div>
             <div class="col-md-2">
                 <label class="form-check-label" for="flexCheckIndeterminate">Largura
                     <span><strong>*</strong></span> </label>
-                <input type="number" class="form-control form-control-sm" name="nome" placeholder="" required>
+                <input type="number" class="form-control form-control-sm" name="largura03_oms" placeholder="" required>
             </div>
             <div class="text-end" style="margin-top: 20px">
                 <input type="submit" class="btn btn-success" value="Salvar Registros">
@@ -280,7 +280,7 @@
 
 </form>
 
-<form action="" method="GET"  enctype="multipart/form-data" style="margin-bottom: 300px">
+<form action="" method="GET"  enctype="multipart/form-data">
 
     <div class="row g-3 shadow p-4 mb-5 bg-body rounded" style="margin-top: 80px">
         <div class="text-center">
@@ -293,17 +293,17 @@
             <div class="col-md-4" id="selecionarFoto">
                 <label class="form-check-label" for="image">Foto do banner principal</label>
                 <!--<input class="form-control form-control-sm" type="file" name="foto" id="formFile"> -->
-                <input type="file" name="foto" id="formFile" class="form-control-file">
+                <input type="file" name="foto_utv" id="formFile" class="form-control-file">
             </div>
             <div class="col-md-2">
                 <label class="form-check-label" for="flexCheckIndeterminate">Altura
                     <span><strong>*</strong></span> </label>
-                <input type="number" class="form-control form-control-sm" name="nome" placeholder="" required>
+                <input type="number" class="form-control form-control-sm" name="altura_utv" placeholder="" required>
             </div>
             <div class="col-md-2">
                 <label class="form-check-label" for="flexCheckIndeterminate">Largura
                     <span><strong>*</strong></span> </label>
-                <input type="number" class="form-control form-control-sm" name="nome" placeholder="" required>
+                <input type="number" class="form-control form-control-sm" name="largura_utv" placeholder="" required>
             </div>
 
         </div>
@@ -314,17 +314,17 @@
             <div class="col-md-4" id="selecionarFoto">
                 <label class="form-check-label" for="image">Foto do banner principal</label>
                 <!--<input class="form-control form-control-sm" type="file" name="foto" id="formFile"> -->
-                <input type="file" name="foto" id="formFile" class="form-control-file">
+                <input type="file" name="foto_slide1_utv" id="formFile" class="form-control-file">
             </div>
             <div class="col-md-2">
                 <label class="form-check-label" for="flexCheckIndeterminate">Altura
                     <span><strong>*</strong></span> </label>
-                <input type="number" class="form-control form-control-sm" name="nome" placeholder="" required>
+                <input type="number" class="form-control form-control-sm" name="altura1_sliede_utv" placeholder="" required>
             </div>
             <div class="col-md-2">
                 <label class="form-check-label" for="flexCheckIndeterminate">Largura
                     <span><strong>*</strong></span> </label>
-                <input type="number" class="form-control form-control-sm" name="nome" placeholder="" required>
+                <input type="number" class="form-control form-control-sm" name="largura1_sliede_utv" placeholder="" required>
             </div>
 
         </div>
@@ -335,17 +335,17 @@
             <div class="col-md-4" id="selecionarFoto">
                 <label class="form-check-label" for="image">Foto do banner principal</label>
                 <!--<input class="form-control form-control-sm" type="file" name="foto" id="formFile"> -->
-                <input type="file" name="foto" id="formFile" class="form-control-file">
+                <input type="file" name="foto2_slide_utv" id="formFile" class="form-control-file">
             </div>
             <div class="col-md-2">
                 <label class="form-check-label" for="flexCheckIndeterminate">Altura
                     <span><strong>*</strong></span> </label>
-                <input type="number" class="form-control form-control-sm" name="nome" placeholder="" required>
+                <input type="number" class="form-control form-control-sm" name="altura2_sliede_utv" placeholder="" required>
             </div>
             <div class="col-md-2">
                 <label class="form-check-label" for="flexCheckIndeterminate">Largura
                     <span><strong>*</strong></span> </label>
-                <input type="number" class="form-control form-control-sm" name="nome" placeholder="" required>
+                <input type="number" class="form-control form-control-sm" name="largura2_sliede_utv" placeholder="" required>
             </div>
         </div>
 
@@ -355,17 +355,17 @@
             <div class="col-md-4" id="selecionarFoto">
                 <label class="form-check-label" for="image">Foto do banner principal</label>
                 <!--<input class="form-control form-control-sm" type="file" name="foto" id="formFile"> -->
-                <input type="file" name="foto" id="formFile" class="form-control-file">
+                <input type="file" name="foto3_slide_utv" id="formFile" class="form-control-file">
             </div>
             <div class="col-md-2">
                 <label class="form-check-label" for="flexCheckIndeterminate">Altura
                     <span><strong>*</strong></span> </label>
-                <input type="number" class="form-control form-control-sm" name="nome" placeholder="" required>
+                <input type="number" class="form-control form-control-sm" name="altura3_sliede_utv" placeholder="" required>
             </div>
             <div class="col-md-2">
                 <label class="form-check-label" for="flexCheckIndeterminate">Largura
                     <span><strong>*</strong></span> </label>
-                <input type="number" class="form-control form-control-sm" name="nome" placeholder="" required>
+                <input type="number" class="form-control form-control-sm" name="largura3_sliede_utv" placeholder="" required>
             </div>
             <div class="text-end" style="margin-top: 20px">
                 <input type="submit" class="btn btn-success" value="Salvar Registros">
