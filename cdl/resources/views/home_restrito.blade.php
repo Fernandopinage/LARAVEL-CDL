90d754635c95a3348e596f8b57afffb773f2f409
FernandoPinage <luizfernandoluck@hotmail.com>
2021-11-03 09:37:20 -0400

    ok

--- cdl/resources/views/home_restrito.blade.php
+++ cdl/resources/views/home_restrito.blade.php
@@ -2,7 +2,7 @@
 
 @section('home')
 
-<form action="">
+<form action=""  enctype="multipart/form-data" style="margin-bottom: 300px">
 
     <div class="row g-3 shadow p-4 mb-5 bg-body rounded" style="margin-top: 80px">
         <div class="text-center">
@@ -26,15 +26,77 @@
                     <span><strong>*</strong></span> </label>
                 <input type="number" class="form-control form-control-sm" name="nome" placeholder="" required>
             </div>
+        </div>
+        <div class="row g-3">
+            <h5>Slide 01</h5>
+            <hr>
+            <div class="col-md-4" id="selecionarFoto">
+                <label class="form-check-label" for="image">Foto do banner principal</label>
+                <!--<input class="form-control form-control-sm" type="file" name="foto" id="formFile"> -->
+                <input type="file" name="foto" id="formFile" class="form-control-file">
+            </div>
+            <div class="col-md-2">
+                <label class="form-check-label" for="flexCheckIndeterminate">Altura
+                    <span><strong>*</strong></span> </label>
+                <input type="number" class="form-control form-control-sm" name="nome" placeholder="" required>
+            </div>
+            <div class="col-md-2">
+                <label class="form-check-label" for="flexCheckIndeterminate">Largura
+                    <span><strong>*</strong></span> </label>
+                <input type="number" class="form-control form-control-sm" name="nome" placeholder="" required>
+            </div>
+
+        </div>
+
+        <div class="row g-3">
+            <h5>Slide 02</h5>
+            <hr>
+            <div class="col-md-4" id="selecionarFoto">
+                <label class="form-check-label" for="image">Foto do banner principal</label>
+                <!--<input class="form-control form-control-sm" type="file" name="foto" id="formFile"> -->
+                <input type="file" name="foto" id="formFile" class="form-control-file">
+            </div>
+            <div class="col-md-2">
+                <label class="form-check-label" for="flexCheckIndeterminate">Altura
+                    <span><strong>*</strong></span> </label>
+                <input type="number" class="form-control form-control-sm" name="nome" placeholder="" required>
+            </div>
+            <div class="col-md-2">
+                <label class="form-check-label" for="flexCheckIndeterminate">Largura
+                    <span><strong>*</strong></span> </label>
+                <input type="number" class="form-control form-control-sm" name="nome" placeholder="" required>
+            </div>
+        </div>
+
+        <div class="row g-3">
+            <h5>Slide 03</h5>
+            <hr>
+            <div class="col-md-4" id="selecionarFoto">
+                <label class="form-check-label" for="image">Foto do banner principal</label>
+                <!--<input class="form-control form-control-sm" type="file" name="foto" id="formFile"> -->
+                <input type="file" name="foto" id="formFile" class="form-control-file">
+            </div>
+            <div class="col-md-2">
+                <label class="form-check-label" for="flexCheckIndeterminate">Altura
+                    <span><strong>*</strong></span> </label>
+                <input type="number" class="form-control form-control-sm" name="nome" placeholder="" required>
+            </div>
+            <div class="col-md-2">
+                <label class="form-check-label" for="flexCheckIndeterminate">Largura
+                    <span><strong>*</strong></span> </label>
+                <input type="number" class="form-control form-control-sm" name="nome" placeholder="" required>
+            </div>
             <div class="text-end" style="margin-top: 20px">
                 <input type="submit" class="btn btn-success" value="Salvar Registros">
             </div>
         </div>
+
+
     </div>
 
 </form>
 
-<form action="">
+<form action=""  enctype="multipart/form-data" style="margin-bottom: 300px">
 
     <div class="row g-3 shadow p-4 mb-5 bg-body rounded" style="margin-top: 80px">
         <div class="text-center">
@@ -66,7 +128,7 @@
 
 </form>
 
-<form action="">
+<form action=""  enctype="multipart/form-data" style="margin-bottom: 300px">
 
     <div class="row g-3 shadow p-4 mb-5 bg-body rounded" style="margin-top: 80px">
         <div class="text-center">
@@ -122,7 +184,7 @@
 </form>
 
 
-<form action="">
+<form action=""  enctype="multipart/form-data" style="margin-bottom: 300px">
 
     <div class="row g-3 shadow p-4 mb-5 bg-body rounded" style="margin-top: 80px">
         <div class="text-center">
@@ -218,7 +280,7 @@
 
 </form>
 
-<form action="">
+<form action=""  enctype="multipart/form-data" style="margin-bottom: 300px">
 
     <div class="row g-3 shadow p-4 mb-5 bg-body rounded" style="margin-top: 80px">
         <div class="text-center">
