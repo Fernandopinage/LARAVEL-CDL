31eca8a4bd63a2d633212dd9b81e86004f08de9d
FernandoPinage <luizfernandoluck@hotmail.com>
2022-01-03 15:06:53 -0400

    ok

--- cdl/resources/views/layout/home.blade.php
+++ cdl/resources/views/layout/home.blade.php
@@ -9,8 +9,7 @@
         integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <link href="/css/home.css" rel="stylesheet">
     <link rel="icon" href="/img/title.png">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
-    rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <title>Empregabilidade | CDL Manaus</title>
 </head>
 
@@ -32,9 +31,9 @@
                                 Candidatos
                             </a>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                 <li><a class="dropdown-item" href="/login/candidato">Entrar</a></li>
-                               <!-- <li><a class="dropdown-item" href="">Buscar Vagas</a></li> <!-- "/busca/candidato"  remover quando for implementar -->
-                      <!--<li><hr class="dropdown-divider"></li>-->
+                                <li><a class="dropdown-item" href="/login/candidato">Entrar</a></li>
+                                <!-- <li><a class="dropdown-item" href="">Buscar Vagas</a></li> <!-- "/busca/candidato"  remover quando for implementar -->
+                                <!--<li><hr class="dropdown-divider"></li>-->
 
                             </ul>
                         </li>
@@ -68,9 +67,6 @@
 
                             </ul>
                         </li>
-
-
-
                     </ul>
 
                 </div>
@@ -78,17 +74,22 @@
             </div>
             <ul class="nav justify-content-end">
                 <li class="nav-item">
-                    <a class="nav-link active" target="_blank" aria-current="page" href="https://www.linkedin.com/company/cdlm/"><img src="/icons/1.png" width="25px"></a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link active" target="_blank" aria-current="page" href="https://pt-br.facebook.com/cdlmanausoficial/"><img src="/icons/2.png" width="25px"></a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" target="_blank" href="https://www.instagram.com/cdlmanaus/?hl=en"><img src="/icons/4.png" width="25px"></a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" target="_blank" href="https://www.youtube.com/channel/UCyjD5GbQuRrztgyRxlVAZdQ"><img src="/icons/3.png" width="25px"></a>
-                  </li>
+                    <a class="nav-link active" target="_blank" aria-current="page"
+                        href="https://www.linkedin.com/company/cdlm/"><img src="/icons/1.png" width="25px"></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" target="_blank" aria-current="page"
+                        href="https://pt-br.facebook.com/cdlmanausoficial/"><img src="/icons/2.png" width="25px"></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" target="_blank" href="https://www.instagram.com/cdlmanaus/?hl=en"><img
+                            src="/icons/4.png" width="25px"></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" target="_blank"
+                        href="https://www.youtube.com/channel/UCyjD5GbQuRrztgyRxlVAZdQ"><img src="/icons/3.png"
+                            width="25px"></a>
+                </li>
 
             </ul>
         </div>
@@ -96,14 +97,14 @@
     </nav>
 
 
-        <div class="banner-principal">
-            <div class="container">
-                @yield('conteudo')
-            </div>
+    <div class="banner-principal" class="justi">
+        <div class="container">
+            @yield('conteudo')
         </div>
+    </div>
 
-    <div class="container" id="container" style="">
-        <div class="row gx-6">
+    <div class="container">
+        <div class="row">
             <div class="col-lg-4">
                 <div class="card" style="width: 25rem;  margin-right: 50px;">
                     <img class="card-img-top" src="/img/index-1.jpg" alt="Imagem de capa do card">
@@ -129,7 +130,8 @@
                             empresas. Cadastra-se. É GRÁTIS!</p>
                     </div>
                     <ul class="list-group list-group-flush">
-                        <a href="/login/empresa" class="btn btn-outline-primary btn-lg btn-block">BUSCAR OPORTUNIDADE</a>
+                        <a href="/login/empresa" class="btn btn-outline-primary btn-lg btn-block">BUSCAR
+                            OPORTUNIDADE</a>
                     </ul>
                 </div>
             </div>
@@ -148,51 +150,51 @@
                 </div>
             </div>
         </div>
+    </div>
+    </div>
 
     </div>
-    
-    </div>
     <hr>
-    <footer style="margin-top: 150px; " >
+    <footer style="margin-top: 150px; ">
         <nav class="navbar bottom navbar-expand-sm navbar bg" style="background-color: #d0d0d056; margin-bottom:0px">
-        
-          <div class="container" id="container" style="color:#black; font-size:14px; ">
-              <div class="row">
-                  <div class="col-md-3 p-4">
-                      <h6>> CDL MANAUS - DJALMA BATISTA</h6>
-                      <span style="color: black">Cond. Amazonas Flat Service, 3000<br>
-                          Loja A, Torre Sul |
-                          Av. Djalma Batista | Chapada | 69.050-010<br>
-                          Segunda a Quinta-feira das 08h às 18h<br>
-                          Sexta-feira das 08h às 17h</span>
-                  </div>
-                  <div class="col-md-3 p-4">
-                      <h6>> CDL MANAUS - CENTRO</h6>
-                      <span style="color: black">Rua Rui Barbosa, 156<br>
-                          Centro  |  69.010-220<br>
-                          Segunda a Quinta-feira das 08h às 18h<br>
-                          Sexta-feira das 08h às 17h</span>
-                  </div>
-                  <div class="col-md-3 p-4">
-                      <h6>>
-                          UTV - RAIZ</h6>
-                      <span style="color: black">Rua Delfim de Souza, 125<br>
-                          Raiz  |  69.068-020<br>
-                          Segunda a Quinta-feira das 08h às 18h<br>
-                          Sexta-feira das 08h às 17h</span>
-                  </div>
-                  <div class="col-md-3 p-4">
-                      <h6>> ATENDIMENTO</h6>
-                      <span style="color: black">Dúvidas<br>
-                          + 55 92 3627-2867<br>
-                          + 55 92 3627-2868<br>
-                          suporte@cdlmanaus.org.br</span>
-                  </div>
-              </div>
-        
-    
+
+            <div class="container" id="container" style="color:#black; font-size:14px; ">
+                <div class="row">
+                    <div class="col-md-3 p-4">
+                        <h6>> CDL MANAUS - DJALMA BATISTA</h6>
+                        <span style="color: black">Cond. Amazonas Flat Service, 3000<br>
+                            Loja A, Torre Sul |
+                            Av. Djalma Batista | Chapada | 69.050-010<br>
+                            Segunda a Quinta-feira das 08h às 18h<br>
+                            Sexta-feira das 08h às 17h</span>
+                    </div>
+                    <div class="col-md-3 p-4">
+                        <h6>> CDL MANAUS - CENTRO</h6>
+                        <span style="color: black">Rua Rui Barbosa, 156<br>
+                            Centro | 69.010-220<br>
+                            Segunda a Quinta-feira das 08h às 18h<br>
+                            Sexta-feira das 08h às 17h</span>
+                    </div>
+                    <div class="col-md-3 p-4">
+                        <h6>>
+                            UTV - RAIZ</h6>
+                        <span style="color: black">Rua Delfim de Souza, 125<br>
+                            Raiz | 69.068-020<br>
+                            Segunda a Quinta-feira das 08h às 18h<br>
+                            Sexta-feira das 08h às 17h</span>
+                    </div>
+                    <div class="col-md-3 p-4">
+                        <h6>> ATENDIMENTO</h6>
+                        <span style="color: black">Dúvidas<br>
+                            + 55 92 3627-2867<br>
+                            + 55 92 3627-2868<br>
+                            suporte@cdlmanaus.org.br</span>
+                    </div>
+                </div>
+
+
             </div>
-          </div>
+            </div>
         </nav>
         <!--
         <nav class="navbar fixed-bottom navbar-expand-sm navbar bg" style="background-color: #284D92">
