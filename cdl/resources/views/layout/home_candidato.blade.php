92468d15b3e0d5d74aaf9031651977b8833433ae
FernandoPinage <luizfernandoluck@hotmail.com>
2022-01-05 09:07:34 -0400

    ok

--- cdl/resources/views/layout/home_candidato.blade.php
+++ cdl/resources/views/layout/home_candidato.blade.php
@@ -71,8 +71,8 @@
     </div>
 
 
-    <footer >
-      <nav class="navbar bottom navbar-expand-sm navbar bg" style="background-color: #d0d0d0fa; margin-bottom:0px">
+    <footer style="margin-top: 150px;" >
+      <nav class="navbar bottom navbar-expand-sm navbar bg" style="background-color: #d0d0d056; margin-bottom:0px">
       
         <div class="container" id="container" style="color:#black; font-size:14px;">
             <div class="row">
