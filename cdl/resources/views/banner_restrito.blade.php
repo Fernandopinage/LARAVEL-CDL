f0aff19fc13055a67755e5436f662b3b2c6ac47c
Luiz Fernando Pinagé Coutinho <luizfernandoluck@hotmail.com>
2022-02-18 15:28:11 -0400

    ok

--- cdl/resources/views/banner_restrito.blade.php
+++ cdl/resources/views/banner_restrito.blade.php
@@ -15,7 +15,7 @@
             <div class="col-md-4" id="selecionarFoto">
                 <label class="form-check-label" for="image">PRIMEIRA IMAGEM</label>
                 <!--<input class="form-control form-control-sm" type="file" name="foto" id="formFile"> -->
-                <input type="file" name="foto_principal" id="file01" onchange="checkPhoto(this)" class="form-control-file">
+                <input type="file" name="foto_principal" id="carrossel01" onchange="checkPhoto(this)" class="form-control-file">
             </div>
         </div>
 
@@ -23,7 +23,7 @@
             <div class="col-md-4" id="selecionarFoto">
                 <label class="form-check-label" for="image">SEGUNDA IMAGEM</label>
                 <!--<input class="form-control form-control-sm" type="file" name="foto" id="formFile"> -->
-                <input type="file" name="slide01_principal" id="formFile" class="form-control-file">
+                <input type="file" name="slide01_principal" id="carrossel02" class="form-control-file">
             </div>
         </div>
 
@@ -31,7 +31,7 @@
             <div class="col-md-4" id="selecionarFoto">
                 <label class="form-check-label" for="image">TERCEIRA IMAGEM</label>
                 <!--<input class="form-control form-control-sm" type="file" name="foto" id="formFile"> -->
-                <input type="file" name="slide02_principal" id="formFile" class="form-control-file">
+                <input type="file" name="slide02_principal" id="carrossel03" class="form-control-file">
             </div>
         </div>
 
@@ -42,7 +42,7 @@
             <div class="col-md-2" id="selecionarFoto">
                 <label class="form-check-label" for="image">SLIDE 01</label>
                 <!--<input class="form-control form-control-sm" type="file" name="foto" id="formFile"> -->
-                <input type="file" name="slide03_principal" id="formFile" class="form-control-file">
+                <input type="file" name="slide03_principal" id="SLIDE 01" class="form-control-file">
             </div>
          </div>
 
@@ -51,7 +51,7 @@
             <div class="col-md-2" id="selecionarFoto">
                 <label class="form-check-label" for="image">SLIDE 02</label>
                 <!--<input class="form-control form-control-sm" type="file" name="foto" id="formFile"> -->
-                <input type="file" name="slide03_principal" id="formFile" class="form-control-file">
+                <input type="file" name="slide03_principal" id="SLIDE 02" class="form-control-file">
             </div>
          </div>
 
@@ -60,7 +60,7 @@
             <div class="col-md-2" id="selecionarFoto">
                 <label class="form-check-label" for="image">SLIDE 03</label>
                 <!--<input class="form-control form-control-sm" type="file" name="foto" id="formFile"> -->
-                <input type="file" name="slide03_principal" id="formFile" class="form-control-file">
+                <input type="file" name="slide03_principal" id="SLIDE 03" class="form-control-file">
             </div>
          </div>
 
@@ -204,6 +204,9 @@
                 <!--<input class="form-control form-control-sm" type="file" name="foto" id="formFile"> -->
                 <input type="file" name="foto_slide03_oms" id="formFile" class="form-control-file">
             </div>
+        </div>
+        <div class="text-end">
+            <input type="submit" class="btn btn-success" value="Salvar">
         </div>
 
     </div>
